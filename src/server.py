c83d79706dcc2f5cd2dc6a13ad8efb5bc8dd07bd
Huy Bui <anhhuypro0123@gmail.com>
2021-04-28 23:18:22 +0700

    Add Dockerfile

--- src/server.py
+++ src/server.py
@@ -5,10 +5,22 @@ server = Flask(__name__)
 @server.route("/search")
 
 def search():
-   message = request.args.get("message")
-   for m in message:
-      print(m)
-   return message
+   result = ""
+   string = request.args.get("message")
+   count = 1
+   result += string[0]
+
+   for i in range(len(string)-1):
+      if(string[i] == string[i+1]):
+         count+=1
+      else:
+         if(count > 1):
+               result += str(count)
+         result += string[i+1]
+         count = 1
+   if(count > 1):
+      result += str(count)
+   return result
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
